e173dfbce669e57399039b203abc69c8e47fd7b4
Abraham Sesay <abrahamsesay96@gmail.com>
2020-07-25 16:36:19 -0500

    adding resume to js

---
+++ src/resume.js
@@ -0,0 +1,42 @@
+const resume = [
+    {   
+        id: 1,
+        name: "Abraham Sesay",
+        title: "Computer Science Student"
+    },
+    {
+        id: 2,
+        email: "abrahamsesay96@gmail.com",
+        github: "https://github.com/Absesay",
+    },
+    {
+        id: 3,
+        section: "Education",
+        degree: "Bachelor of Science in Computer Science",
+        school: "St. Cloud State University (SCSU) — St Cloud MN",
+        year: "Dec 2022 "
+    },
+    {
+        id: 4,
+        section: "Technical Skills",
+        langHeading: "Programming Languages",
+        langs: "C++, Java, Python, Javascript(ES6), HTML5, CSS, MySQL, Bash",
+        libsheading: "Libraries & Frameworks",
+        libraries: "Flask, Node.js, Express, MongoDB, Mongoose, React",
+        toolsHeading: "Tools & Platforms",
+        tools: "Git, Heroku, Linux/Unix CLI, Puppet, SaltStack, Docker",
+        classHeading: "Relevant Courses",
+        classes: "Object-Oriented Programming, Recursion, Data Structures  & Algorithms, Computer Architecture, Discrete Math, Calculus"
+    },
+    {   
+        id: 5,
+        section: "Experience",
+        exp1: "Full-stack Web Development online Bootcamp - Udemy",
+        year1: "December 2019 - Present",
+        bulet1: "Implemented  a RESTful API for a wiki service using Node, Express and MongoDB with Mongoose",
+        bulet2:"Used a variety of NPM  packages and Google Auth to encrypt user passwords in a MongoDB database",
+        bulet3:"Built portfolio a website using HTML and CSS and hosted the site using GitHub pages"
+    }
+]
+
+export default resume
