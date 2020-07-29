a053fb2adab3b41835d56918e6b55bc7bfc83d92
Abraham Sesay <abrahamsesay96@gmail.com>
2020-07-29 00:07:23 -0500

    add css

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import React from "react"
 import ReactDom from "react-dom"
 
-ReactDom.render(<h1>Hello World</h1>,document.getElementById("root"))
+ReactDom.render(
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-8">col-8</div>
+      <div className="col-4">col-4</div>
+    </div>
+  </div>,document.getElementById("root"))
