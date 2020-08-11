8aa2f232663ee23e4a48c899e9b94a590577269d
Abraham Sesay <abrahamsesay96@gmail.com>
2020-08-10 21:47:06 -0500

    added content

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import ReactDom from "react-dom"
 ReactDom.render(
   <div className="container-fluid">
     <div className="row">
-      <div className="col-8">col-8</div>
+      <div className="col-8">col-8
+        <div className="">
+
+        </div>
+      </div>
       <div className="col-4">col-4</div>
     </div>
   </div>,document.getElementById("root"))
